3d1bff8888b3f06caffd5bb31b05c74313e17e76
italofvm <italofernandesvm@gmail.com>
2026-01-06 15:38:25 -0300

    updater icon app

--- lib/core/config/app_config.dart
+++ lib/core/config/app_config.dart
@@ -1,4 +1,4 @@
 class AppConfig {
-  static const String githubOwner = 'SEU_USUARIO_OU_ORG';
-  static const String githubRepo = 'SEU_REPO';
+  static const String githubOwner = 'italofvm';
+  static const String githubRepo = 'lumen-reader';
 }
